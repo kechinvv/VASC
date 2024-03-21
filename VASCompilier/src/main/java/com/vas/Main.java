97b1142c94e706bc0f8c9a9e449b14b1ff13c8dc
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-03-21 11:17:02 +0300

    nicer print

--- VASCompilier/src/main/java/com/vas/Main.java
+++ VASCompilier/src/main/java/com/vas/Main.java
@@ -5,7 +5,9 @@ import com.vas.gen.VASCParser;
 import com.vas.gen.VASCParserBaseVisitor;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.misc.Interval;
 import org.antlr.v4.runtime.tree.RuleNode;
 
 import java.io.IOException;
@@ -30,10 +32,23 @@ public class Main {
             @Override
             public Void visitChildren(RuleNode node) {
                 var ruleId = node.getRuleContext().getRuleIndex();
-                if (ruleId >= 0 && ruleId < VASCParser.ruleNames.length) {
-                    var rule = VASCParser.ruleNames[ruleId];
-                    int endIndex = Math.min(node.getText().length(), 100);
-                    System.out.println(rule + " ".repeat(30 - rule.length()) + node.getText().substring(0, endIndex));
+                if (ruleId >= 0 && ruleId < VASCParser.ruleNames.length && node instanceof ParserRuleContext ctx) {
+                    try {
+                        String text;
+                        if (ctx.start == null || ctx.stop == null || ctx.start.getStartIndex() < 0 || ctx.stop.getStopIndex() < 0)
+                            text = ctx.getText();
+                        else
+                            text = ctx.start.getInputStream().getText(Interval.of(ctx.start.getStartIndex(), ctx.stop.getStopIndex()));
+                        var rule = VASCParser.ruleNames[((ParserRuleContext) node).getRuleIndex()];
+                        var lines = text.split("\n");
+                        var maxIndent = 30;
+                        lines[0] = ("|" + lines[0].indent(ctx.start.getCharPositionInLine())).indent(maxIndent - rule.length());
+                        for (int i = 1; i < lines.length; i++) {
+                            lines[i] = ("|" + lines[i]).indent(maxIndent);
+                        }
+                        System.out.println(rule + String.join("", lines));
+                    } catch (Exception ignored) {
+                    }
                 }
                 return super.visitChildren(node);
             }
