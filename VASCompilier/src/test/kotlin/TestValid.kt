b11a1b34c666ceeb429d32c53790ad0360f0ced5
Vyacheslav Kovalevsky <40753025+Slava0135@users.noreply.github.com>
2024-03-25 17:10:13 +0300

    use test factory (#9)
    * use test factory
    
    * fix formatting
    
    * use List.first()

--- VASCompilier/src/test/kotlin/TestValid.kt
+++ VASCompilier/src/test/kotlin/TestValid.kt
@@ -1,55 +1,45 @@
 import com.vas.antlr.VASCLexer
 import com.vas.antlr.VASCParser
 import org.antlr.v4.runtime.*
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.api.fail
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
 import java.io.File
-import java.util.stream.Stream
 
 class TestValid {
-    companion object {
-        @JvmStatic
-        fun provideSources(): Stream<Arguments> {
-            val dirPath = this::class.java.classLoader.resources("valid").iterator().next()
-            val dir = File(dirPath.path)
-            return dir.listFiles()!!.map {
-                Arguments.of(object : File(it.path) {
-                    override fun toString() = nameWithoutExtension
-                })
-            }.stream()
-        }
-    }
-
-    @ParameterizedTest(name = "{argumentsWithNames}")
-    @MethodSource("provideSources")
-    fun `test valid code`(file: File) {
-        val lexer = VASCLexer(CharStreams.fromString(file.readText()))
-        val parser = VASCParser(CommonTokenStream(lexer))
-        val errorListener = object : BaseErrorListener() {
-            val errors = mutableListOf<String>()
-            override fun syntaxError(
-                recognizer: Recognizer<*, *>?,
-                offendingSymbol: Any?,
-                line: Int,
-                charPositionInLine: Int,
-                msg: String?,
-                e: RecognitionException?
-            ) {
-                val detailedMsg = """
-                    |parser error at $line:$charPositionInLine
-                    |$msg
-                """.trimMargin()
-                errors.add(detailedMsg)
+    @TestFactory
+    fun `test valid code`(): Collection<DynamicTest> {
+        val dirPath = this::class.java.classLoader.resources("valid").toList().first()
+        val dir = File(dirPath.path)
+        return dir.listFiles()!!.map { file ->
+            DynamicTest.dynamicTest(file.nameWithoutExtension) {
+                val lexer = VASCLexer(CharStreams.fromString(file.readText()))
+                val parser = VASCParser(CommonTokenStream(lexer))
+                val errorListener = object : BaseErrorListener() {
+                    val errors = mutableListOf<String>()
+                    override fun syntaxError(
+                        recognizer: Recognizer<*, *>?,
+                        offendingSymbol: Any?,
+                        line: Int,
+                        charPositionInLine: Int,
+                        msg: String?,
+                        e: RecognitionException?
+                    ) {
+                        val detailedMsg = """
+                            |parser error at $line:$charPositionInLine
+                            |$msg
+                        """.trimMargin()
+                        errors.add(detailedMsg)
+                    }
+                }
+                parser.removeErrorListeners()
+                parser.addErrorListener(errorListener)
+                parser.program()
+                if (errorListener.errors.isNotEmpty()) {
+                    System.err.println(errorListener.errors.joinToString("\n\n"))
+                    fail("unexpected parser errors in (${file.name}:1)")
+                }
             }
-        }
-        parser.removeErrorListeners()
-        parser.addErrorListener(errorListener)
-        parser.program()
-        if (errorListener.errors.isNotEmpty()) {
-            System.err.println(errorListener.errors.joinToString("\n\n"))
-            fail("unexpected parser errors in (${file.name}:1)")
         }
     }
 }
