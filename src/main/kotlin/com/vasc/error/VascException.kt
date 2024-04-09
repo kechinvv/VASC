74c2c971d262a70e7851615cd8f357faaa688851
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-04-09 23:04:51 +0300

    more context in errors

--- src/main/kotlin/com/vasc/error/VascException.kt
+++ src/main/kotlin/com/vasc/error/VascException.kt
@@ -4,19 +4,47 @@ import org.antlr.v4.runtime.ParserRuleContext
 import org.antlr.v4.runtime.misc.Interval
 
 open class VascException(
-    description: String,
-    ctx: ParserRuleContext? = null
+    description: String, ctx: ParserRuleContext? = null
 ) : RuntimeException(description + prettyPosition(ctx))
 
 private fun prettyPosition(ctx: ParserRuleContext?): String {
     ctx ?: return ""
-    val source = ctx.start.inputStream.getText(Interval.of(ctx.start.startIndex, ctx.stop.stopIndex))
+    return when (val parentCtx = ctx.parent) {
+        is ParserRuleContext -> prettyPositionWithinParent(ctx, parentCtx)
+        else -> prettyPositionNoParent(ctx)
+    }
+}
+
+private fun prettyPositionNoParent(ctx: ParserRuleContext): String {
+    val source = getSourceText(ctx)
     val indent = " ".repeat(ctx.start.charPositionInLine)
-    val pointers = "^".repeat(source.length)
+    val pointers = "^".repeat(source.lines().maxOfOrNull { it.length }!!)
     return """
         |
-        |at [${ctx.start.line}:${ctx.start.charPositionInLine}]
+        |${at(ctx)}
         |$indent$source
         |$indent$pointers
     """.trimMargin()
 }
+
+private fun prettyPositionWithinParent(ctx: ParserRuleContext, parentCtx: ParserRuleContext): String {
+    val source = getSourceText(ctx)
+    val parentSource = getSourceText(parentCtx)
+    val indent = " ".repeat(ctx.start.charPositionInLine)
+    val indentParent = " ".repeat(parentCtx.start.charPositionInLine)
+    val (before, after) = parentSource.lines().mapIndexed { n, line -> Pair(n, line) }
+        .partition { it.first + parentCtx.start.line <= ctx.start.line }
+    val pointers = "^".repeat(source.lines().last().length)
+    return """
+        |
+        |${at(ctx)}
+        |$indentParent${before.joinToString("\n") { it.second }}  
+        |$indent$pointers
+        |${after.joinToString("\n") { it.second }}
+    """.trimMargin()
+}
+
+private fun getSourceText(ctx: ParserRuleContext) =
+    ctx.start.inputStream.getText(Interval.of(ctx.start.startIndex, ctx.stop.stopIndex))
+
+private fun at(ctx: ParserRuleContext) = "at [${ctx.start.line}:${ctx.start.charPositionInLine}]"
