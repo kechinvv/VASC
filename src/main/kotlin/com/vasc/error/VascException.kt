5c72c6891aa263089c446f206598f2c0f6bb4f16
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-04-09 20:18:02 +0300

    put source in error msg

--- src/main/kotlin/com/vasc/error/VascException.kt
+++ src/main/kotlin/com/vasc/error/VascException.kt
@@ -1,8 +1,22 @@
 package com.vasc.error
 
 import org.antlr.v4.runtime.ParserRuleContext
+import org.antlr.v4.runtime.misc.Interval
 
 open class VascException(
-    private val decsription: String,
-    private val ctx: ParserRuleContext? = null
-) : RuntimeException(decsription + (ctx?.let { "\nat [${ctx.start.line}:${ctx.start.charPositionInLine}]" } ?: ""))
+    description: String,
+    ctx: ParserRuleContext? = null
+) : RuntimeException(description + prettyPosition(ctx))
+
+private fun prettyPosition(ctx: ParserRuleContext?): String {
+    ctx ?: return ""
+    val source = ctx.start.inputStream.getText(Interval.of(ctx.start.startIndex, ctx.stop.stopIndex))
+    val indent = " ".repeat(ctx.start.charPositionInLine)
+    val pointers = "^".repeat(source.length)
+    return """
+        |
+        |at [${ctx.start.line}:${ctx.start.charPositionInLine}]
+        |$indent$source
+        |$indent$pointers
+    """.trimMargin()
+}
