fafbf1c81a0040cbfa88b1bf5c30dd0ebc37c8bd
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-04-19 16:19:18 +0300

    generateBody (wip)

--- src/main/kotlin/com/vasc/codegen/generateClass.kt
+++ src/main/kotlin/com/vasc/codegen/generateClass.kt
@@ -8,18 +8,22 @@ import com.vasc.member.VascMethod
 import com.vasc.member.VascVariable
 import com.vasc.type.*
 import com.vasc.util.toUniqueVariables
+import org.antlr.v4.runtime.ParserRuleContext
 
 private const val classPrefix = "vasc/"
 
 class ClassCodeGenerator(private val typeResolver: VascTypeResolver, private val errors: MutableList<VascException>) {
     private val code = StringBuilder()
 
+    private val indentStep = 2
+    private var indent = 0
+
     private fun appendLine() {
         code.appendLine()
     }
 
     private fun appendLine(line: String) {
-        code.appendLine(line)
+        code.appendLine(" ".repeat(indent) + line)
     }
 
     override fun toString() = code.toString()
@@ -39,20 +43,21 @@ class ClassCodeGenerator(private val typeResolver: VascTypeResolver, private val
     }
 
     private fun generateClassBody(vascClass: VascClass, ctx: ClassBodyContext) {
+        appendLine("; %%% Fields %%%")
         val fieldInits = ctx.memberDeclarations.filterIsInstance<FieldDeclarationContext>().map {
             generateField(
                 vascClass.getDeclaredField(it.variableDeclaration().identifier().text)!!,
                 it
             )
         }
-        appendLine()
+        appendLine("; %%% Constructors %%%")
         ctx.memberDeclarations.filterIsInstance<ConstructorDeclarationContext>().forEach { ctor ->
             generateConstructor(
                 vascClass.getDeclaredConstructor(ctor.parameters().toUniqueVariables(typeResolver).map { it.type })!!,
                 ctor
             )
         }
-        appendLine()
+        appendLine("; %%% Methods %%%")
         ctx.memberDeclarations.filterIsInstance<MethodDeclarationContext>().forEach { method ->
             generateMethod(
                 vascClass.getDeclaredMethod(method.identifier().text, method.parameters().toUniqueVariables(typeResolver).map { it.type })!!,
@@ -82,6 +87,64 @@ class ClassCodeGenerator(private val typeResolver: VascTypeResolver, private val
     }
 
     private fun generateBody(ctx: BodyContext) {
+        indent += indentStep
+        for (st in ctx.statement()) {
+            when (st) {
+                is IfStatementContext -> {
+                    generateExpression(st.condition)
+                    val endLabel = "If_End_${st.pos()}"
+                    if (st.elseBody != null) {
+                        val elseLabel = "If_Else_${st.pos()}"
+                        appendLine("ifne $elseLabel")
+                        generateBody(st.thenBody)
+
+                        appendLine("goto $endLabel")
+
+                        appendLine("$elseLabel:")
+                        generateBody(st.elseBody)
+
+                        appendLine("$endLabel:")
+                    } else {
+                        appendLine("ifne $endLabel")
+                        generateBody(st.thenBody)
+                        appendLine("$endLabel:")
+                    }
+                }
+                is WhileStatementContext -> {
+                    val condLabel = "While_Cond_${st.pos()}"
+                    appendLine("$condLabel:")
+
+                    generateExpression(st.condition)
+                    val endLabel = "While_End_${st.pos()}"
+                    appendLine("ifne $endLabel")
+
+                    generateBody(st.body())
+
+                    appendLine("goto $condLabel")
+                    appendLine("$endLabel:")
+                }
+                is AssignStatementContext -> {
+                    appendLine("; TODO: assign")
+                }
+                is ReturnStatementContext -> {
+                    appendLine("return")
+                }
+                is ExpressionStatementContext -> {
+                    generateExpression(st.expression())
+                }
+                is PrintStatementContext -> {
+                    appendLine("; TODO: print")
+                }
+                is VariableStatementContext -> {
+                    appendLine("; TODO: var")
+                }
+            }
+        }
+        indent -= indentStep
+    }
+
+    private fun generateExpression(ctx: ExpressionContext) {
+        appendLine("; TODO: expression")
     }
 
 }
@@ -96,3 +159,5 @@ private fun VascType.toJType(): String {
         else -> throw IllegalArgumentException(this.toString())
     }
 }
+
+private fun ParserRuleContext.pos() = "${start.line}_${start.charPositionInLine}"
