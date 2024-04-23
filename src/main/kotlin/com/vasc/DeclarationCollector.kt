95b54ff6c65b03fa1b917d44042b9ff379626adf
ArtemUntila <untila_artem@mail.ru>
2024-04-24 00:11:47 +0300

    Incompatible overridden method

--- src/main/kotlin/com/vasc/DeclarationCollector.kt
+++ src/main/kotlin/com/vasc/DeclarationCollector.kt
@@ -126,6 +126,12 @@ private class MutableVascClass(name: String) : VascClass(name) {
     }
 
     fun addMethod(method: VascMethod) {
+        // method is considered overridden if parameter types match exactly
+        parent?.methods?.find { method == it }?.let {
+            if (!it.returnType.isAssignableFrom(method.returnType)) {
+                throw VascException("Return type of '$name.$method' is incompatible with parent")
+            }
+        }
         if (!declaredMethods.add(method)) {
             throw VascException("Duplicate method '$name.${method.name}'")
         }
